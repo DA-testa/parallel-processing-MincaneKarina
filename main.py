d38c762abeeebb002347d688d348ff5aa374019a
MincaneKarina <karina.mincane@edu.rtu.lv>
2023-03-14 08:58:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,14 +11,24 @@ def main():
     # TODO: create input from keyboard
     # input consists of two lines
     # first line - n and m
+    firstLine = input()
     # n - thread count 
     # m - job count
-    n = 0
-    m = 0
+    
+    a  = firstLine.split()
+    n = int(a[0])
+    m = int(a[1])
+    #n = 0
+    #m = 0
+    print(n)
+    print(m)
 
     # second line - data 
+    secondLine = input()
+    data = list(map(int, secondLine.split()))
+    print(data)
     # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
-    data = []
+    #data = []
 
     # TODO: create the function
     result = parallel_processing(n,m,data)
